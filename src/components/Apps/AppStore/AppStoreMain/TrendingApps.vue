95dd913826e74d3982ee6ab1bbfcc7353e7ddae5
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2021-08-06 22:06:07 +0530

    🎉

--- src/components/Apps/AppStore/AppStoreMain/TrendingApps.vue
+++ src/components/Apps/AppStore/AppStoreMain/TrendingApps.vue
@@ -81,29 +81,43 @@ export default {
 }
 
 .trendingApps .apps .item:nth-child(1) {
+  --appName: 'Telegram';
+  --desc: 'Social';
   background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1024px-Telegram_logo.svg.png');
 }
 .trendingApps .apps .item:nth-child(2) {
+  --appName: 'Whatsapp';
+  --desc: 'Social';
   background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png');
 }
 .trendingApps .apps .item:nth-child(3) {
+  --appName: 'Visual Studio Code';
+  --desc: 'Developer Tools';
   background-image: url('https://dashboard.snapcraft.io/site_media/appmedia/2019/05/code_ozwVHSV.png');
 }
 .trendingApps .apps .item:nth-child(4) {
+  --appName: 'Adobe Lightroom';
+  --desc: 'Photo & Video';
   background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Adobe_Photoshop_Lightroom_CC_logo.svg/1200px-Adobe_Photoshop_Lightroom_CC_logo.svg.png');
 }
 .trendingApps .apps .item:nth-child(5) {
+  --appName: 'Figma';
+  --desc: 'Productivity';
   background-image: url('https://cdn.sanity.io/images/599r6htc/production/46a76c802176eb17b04e12108de7e7e0f3736dc6-1024x1024.png?w=670&h=670&q=75&fit=max&auto=format');
+}
+.trendingApps .apps .item:nth-child(6) {
+  --appName: 'Github Desktop';
+  --desc: 'Productivity';
+  background-image: url('https://user-images.githubusercontent.com/1784648/97818784-c4fc1080-1ca4-11eb-8681-56ec3fa21756.png');
 }
 
 .trendingApps .apps .item {
   background: rgba(10, 10, 10, 0.623);
-  background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1024px-Telegram_logo.svg.png');
-  background-size: 50%;
+  background-size: 45%;
   background-repeat: no-repeat;
   background-position: center 20%;
 
-
+  position: relative;
   height: 100%;
   width: 16%;
   border-radius: 5px;
@@ -112,6 +126,38 @@ export default {
 
   /* box-sizing: border-box; */
 }
+.trendingApps .apps .item::before {
+    position: absolute;
+    content: var(--appName);
+    font-size: 0.9rem;
+    color: #d6d6d6;
+    font-weight: 600;
+    top: 65%;
+    left: 50%;
+    transform: translateX(-50%);
+}
+.trendingApps .apps .item::before {
+    position: absolute;
+    content: var(--appName);
+    font-size: 0.9rem;
+    color: #d6d6d6;
+    font-weight: 600;
+    top: 65%;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
+.apps .item::after {
+    position: absolute;
+    content: var(--desc);
+    font-size: 0.7rem;
+    color: #d6d6d699;
+    font-weight: 500;
+    top: 78%;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
 .trendingApps .apps .item:hover {
   transform: scale(1.02);
   background-color: #111111;
